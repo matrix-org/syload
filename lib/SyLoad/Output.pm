0d2c7ef596c17cf9b3b00d25ae0e0a957f0ddd94
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2015-05-13 18:29:08 +0100

    Neater implementation of SyLoad::Output using subclassing instead of bizarre closure capture

--- lib/SyLoad/Output.pm
+++ lib/SyLoad/Output.pm
@@ -9,31 +9,10 @@ sub open
    my $class = shift;
    my ( $path ) = @_;
 
-   my $cb;
-
    my $outfh;
    for( $path ) {
       when( [ qr/\.csv$/, qr/\.dat$/ ] ) {
-         my $sep = ( $path =~ m/\.csv$/ ) ? ", " : " ";
-         my $isfirst = 1;
-         my $cumulative_total = 0;
-         $cb = sub {
-            my ( $batch, @buckets ) = split m/\s+/, $_[0];
-            my $time = $_[1];
-
-            if( $isfirst ) {
-               # column headings
-               my @names = map { ( m/^(.*?)=/ )[0] } @buckets;
-               $outfh->print( "# ", join( $sep, "time", "total", "batch", @names ), "\n" );
-
-               undef $isfirst;
-            }
-
-            $_ = ( m/=(.*)/ )[0] for $batch, @buckets;
-
-            $cumulative_total += $batch;
-            $outfh->print( join( $sep, $time, $cumulative_total, $batch, @buckets ), "\n" );
-         };
+         $class = ( $path =~ m/\.csv$/ ) ? "SyLoad::Output::CSV" : "SyLoad::Output::DAT";
       }
       default {
          die "Unsure how to output to a file called $path\n";
@@ -43,13 +22,52 @@ sub open
    open $outfh, ">", $path or die "Cannot open $path for writing - $!\n";
    $outfh->autoflush(1);
 
-   return bless { cb => $cb }, $class;
+   return bless {
+      fh => $outfh,
+
+      isfirst => 1,
+      cumulative_total => 0,
+   }, $class;
 }
 
 sub write
 {
    my $self = shift;
-   $self->{cb}->( @_ );
+
+   my ( $batch, @buckets ) = split m/\s+/, $_[0];
+   my $time = $_[1];
+
+   if( $self->{isfirst} ) {
+      # column headings
+      my @names = map { ( m/^(.*?)=/ )[0] } @buckets;
+      $self->_write_fields( "# time", "total", "batch", @names );
+
+      undef $self->{isfirst};
+   }
+
+   $_ = ( m/=(.*)/ )[0] for $batch, @buckets;
+
+   $self->{cumulative_total} += $batch;
+   $self->_write_fields( $time, $self->{cumulative_total}, $batch, @buckets );
+}
+
+package SyLoad::Output::CSV;
+use base qw( SyLoad::Output );
+
+sub _write_fields
+{
+   my $self = shift;
+   $self->{fh}->print( join( ", ", @_ ), "\n" );
+}
+
+package SyLoad::Output::DAT;
+# A space-separated format for gnuplot to use
+use base qw( SyLoad::Output );
+
+sub _write_fields
+{
+   my $self = shift;
+   $self->{fh}->print( join( " ", @_ ), "\n" );
 }
 
 1;
